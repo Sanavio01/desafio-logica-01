c96922aadacbfe38716124526c73c54fc5b98e78
Sanavio01 <sanavioguitar@gmail.com>
2023-12-24 16:50:32 -0300

    first project

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+/*let i = 1
+while (i <= 10) {
+    console.log ("Subiu para " + i )
+    i++
+}*/
+
+/*let i = 1
+do {
+    console.log ("Subiu para " + i )
+    i++
+} while (i <= 100) */  
+
+/*for (let i = 1; i <= 100; i++) {
+    console.log ("Subiu para " + i )
+}*/
